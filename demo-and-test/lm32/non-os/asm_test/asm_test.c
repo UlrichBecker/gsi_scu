a6a8df02f0d780c4c1b73d8f43cb76f498297c95
Ulrich Becker <>
2022-09-15 16:28:32 +0200

    Current state

--- demo-and-test/lm32/non-os/asm_test/asm_test.c
+++ demo-and-test/lm32/non-os/asm_test/asm_test.c
@@ -1,60 +1,114 @@
 
 #include <mprintf.h>
 #include <stdint.h>
+#include <helper_macros.h>
 
-volatile static uint32_t counter = 0;
+volatile extern uint32_t __atomic_section_nesting_count;
 
-void sig( uint32_t x )
+#define _IRQ_IE 0x00000001
+#define IRQ_IE ((uint32_t)_IRQ_IE)
+
+OPTIMIZE( "O0" )
+void print( uint32_t x )
 {
    mprintf( "x = %u\n" );
 }
 
-#if 1
-void incCounter( void )
+STATIC inline ALWAYS_INLINE uint32_t irqGetEnableRegister( void )
 {
-#if 0
-   asm volatile ( "orhi r1, r0, hi(counter) \n\t"
-                  "ori  r1, r1, lo(counter) \n\t"
-                  "lw       r2, (r1+0)     \n\t"
-                  "lw       r1, (r2+0)     \n\t"
-                  "addi     r1, r1, 1      \n\t"
-                  "sw       (r2+0), r1     \n\t"
-                  :"=r"(counter)
-                  :"r"(counter)
-                  : "r1", "r2"
-                );
-#else
-   counter++;
-#endif
+   uint32_t ie;
+   asm volatile ( "rcsr %0, ie" :"=r"(ie) );
+   return ie;
 }
 
-void decCounter( void )
+
+#if 1
+OPTIMIZE( "O1" )
+void criticalSectionEnter( void )
 {
-   asm volatile ( "orhi r1, r0, hi(counter) \n\t"
-                  "ori  r1, r1, lo(counter) \n\t"
-                  "lw       r2, (r1+0)     \n\t"
-                  "lw       r1, (r2+0)     \n\t"
-                  "addi     r1, r1, -1     \n\t"
-                  "sw       (r2+0), r1     \n\t"
-                  ://"=r"(counter)
-                  ://"r"(counter)
-                  : "r1", "r2" );
+   asm volatile
+   ( 
+      ".long    __atomic_section_nesting_count             \n\t"
+   #ifndef CONFIG_DISABLE_CRITICAL_SECTION
+      "wcsr     ie, r0                                     \n\t"
+   #endif
+      "orhi     r1, r0, hi(__atomic_section_nesting_count) \n\t"
+      "ori      r1, r1, lo(__atomic_section_nesting_count) \n\t"
+      "lw       r2, (r1+0)                                 \n\t"
+      "addi     r2, r2, 1                                  \n\t"
+      "sw       (r1+0), r2                                 \n\t"
+      :
+      :
+      : "r1", "r2"
+   );
+}
+
+#define CONFIG_IRQ_ENABLE_IF_COUNTER_ALREADY_ZERO
+
+OPTIMIZE( "O1" )
+void criticalSectionExit( void )
+{
+   asm volatile
+   (  
+      ".long    __atomic_section_nesting_count             \n\t"
+      "orhi     r1, r0, hi(__atomic_section_nesting_count) \n\t"
+      "ori      r1, r1, lo(__atomic_section_nesting_count) \n\t"
+      "lw       r2, (r1+0)                                 \n\t"
+   #ifdef CONFIG_IRQ_ALSO_ENABLE_IF_COUNTER_ALREADY_ZERO
+      "be       r2, r0, L_ENABLE                           \n\t"
+   #else
+      "be       r2, r0, L_NO_ENABLE                        \n\t"
+   #endif
+      "addi     r2, r2, -1                                 \n\t"
+      "sw       (r1+0), r2                                 \n\t"
+      "bne      r2, r0, L_NO_ENABLE                        \n"
+   "L_ENABLE:                                              \n\t"
+      "ori      r1, r0, " TO_STRING( _IRQ_IE ) "           \n\t"
+      "wcsr     ie, r1                                     \n"
+   "L_NO_ENABLE:                                           \n\t"
+      :
+      :
+      : "r1", "r2" 
+   );
 }
 #endif
 void main( void )
 {
- //  counter = 0;
    mprintf( "Inline assembler test\n" );
-   mprintf( "Noch was...\n" );
+   mprintf( "Address of __atomic_section_nesting_count: 0x%p\n", &__atomic_section_nesting_count );
 
-   mprintf( "counter = %d\n", counter );
+   mprintf( "__atomic_section_nesting_count = %d, enable = 0x%08X, \n",
+             __atomic_section_nesting_count, irqGetEnableRegister() );
 
-   incCounter();
+   criticalSectionEnter();
+   mprintf( "__atomic_section_nesting_count = %d, enable = 0x%08X, \n",
+             __atomic_section_nesting_count, irqGetEnableRegister() );
+
+   criticalSectionEnter();
 #if 1
-   mprintf( "counter = %u\n", counter );
-   decCounter();
-   mprintf( "counter = %u\n", counter );
-   
+   mprintf( "__atomic_section_nesting_count = %d, enable = 0x%08X, \n",
+             __atomic_section_nesting_count, irqGetEnableRegister() );
+
+   criticalSectionExit();
+   mprintf( "__atomic_section_nesting_count = %d, enable = 0x%08X, \n",
+             __atomic_section_nesting_count, irqGetEnableRegister() );
+
+   criticalSectionExit();
+   mprintf( "__atomic_section_nesting_count = %d, enable = 0x%08X, \n",
+             __atomic_section_nesting_count, irqGetEnableRegister() );
+
+   criticalSectionExit();
+   mprintf( "__atomic_section_nesting_count = %d, enable = 0x%08X, \n",
+             __atomic_section_nesting_count, irqGetEnableRegister() );
+
+   criticalSectionExit();
+   mprintf( "__atomic_section_nesting_count = %d, enable = 0x%08X, \n",
+             __atomic_section_nesting_count, irqGetEnableRegister() );
+
+   criticalSectionEnter();
+   mprintf( "__atomic_section_nesting_count = %d, enable = 0x%08X, \n",
+             __atomic_section_nesting_count, irqGetEnableRegister() );
+
 #endif
    while( 1 );
 }
