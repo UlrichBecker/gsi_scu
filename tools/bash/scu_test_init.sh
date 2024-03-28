2d9f6a97e19f4bb584faaa51056f8f6cc1f4173c
Ulrich Becker <u.becker@gsi.de>
2024-03-28 15:49:23 +0100

    Easter Bunny

--- tools/bash/scu_test_init.sh
+++ tools/bash/scu_test_init.sh
@@ -1,20 +1,28 @@
 #!/bin/sh
 ###############################################################################
 ##                                                                           ##
-##    Shellscript to start test applications for testing only                ##
+##               Shell script to start test applications.                    ##
+##          CAUTION: For testing and development purposes only!              ##
 ##                                                                           ##
-##    Make a symbolic link of this script in:                                ##
-##    /opt/nfsinit/<scu-name>                                                ##
 ##---------------------------------------------------------------------------##
 ## File:      scu_test_init.sh                                               ##
 ## Author:    Ulrich Becker                                                  ##
 ## Date:      02.09.2022                                                     ##
 ## Copyright: GSI Helmholtz Centre for Heavy Ion Research GmbH               ##
 ###############################################################################
-
 #
 # Location of this script on ASL: /common/export/nfsinit/global/lm32-test-apps/
 # Location of this script on SCU:           /opt/nfsinit/global/lm32-test-apps/
+#
+# Make a symbolic link of this script in: /opt/nfsinit/<scu-name>:
+#
+# * Symbolic link name for testing the FreeRTOS version "scu3_control_os.bin"
+#   96_scu_test_init
+#
+# * Symbolic link name for testing the classical version "scu3_control.bin"
+#   95_scu_test_init
+#
+# You can easily switch between both versions by renaming of the link-name.
 #
 
 ERROR_LOG=/var/log/error.log
@@ -23,23 +31,41 @@ LM32_APP_DIR=${COMMON_INIT_DIR}lm32-test-apps/
 
 if [ -n "$(echo $0 | grep 96)" ]
 then
-   LM32_APP=scu3_control_os.bin
+   LM32_APP=scu3_control_os.bin   # Version with FreeRTOS (beta!)
 else
-   LM32_APP=scu3_control.bin
+   LM32_APP=scu3_control.bin      # Version without FreeRTOS
 fi
+
+. /etc/functions
 
 EB_FWLOAD=/usr/bin/eb-fwload
 EB_RESET=/usr/bin/eb-reset
+
 LM32_LOGD=${COMMON_INIT_DIR}tools/lm32-logd
+LM32_LOG_TARGET=/var/log/lm32.log
+
 SOCAT=/usr/bin/socat
 
-$EB_RESET dev/wbm0 cpuhalt 0 2>>$ERROR_LOG
-$EB_FWLOAD dev/wbm0 u0 0 ${LM32_APP_DIR}${LM32_APP} 2>>$ERROR_LOG
+if [ -f "${LM32_APP_DIR}${LM32_APP}" ]
+then
+   log "Reseting LM32"
+   $EB_RESET dev/wbm0 cpuhalt 0 2>>$ERROR_LOG
+   log "Upload and starting LM32-application: \"$LM32_APP\""
+   $EB_FWLOAD dev/wbm0 u0 0 ${LM32_APP_DIR}${LM32_APP} 2>>$ERROR_LOG
+   sleep 3
+else
+   log_error "LM32-application: \"${LM32_APP_DIR}${LM32_APP}\" not found!"
+fi
 
-sleep 3
+log "Starting LM32-logging-daemon, target: \"${LM32_LOG_TARGET}\""
+$LM32_LOGD -Habd=${LM32_LOG_TARGET} 2>>$ERROR_LOG
 
-$LM32_LOGD -Habd=/var/log/lm32.log 2>>$ERROR_LOG
-
+log "Starting \"socat\""
 $SOCAT tcp-listen:60368,reuseaddr,fork file:/dev/wbm0 2>>$ERROR_LOG
+
+if [ -f "$ERROR_LOG" ]
+then
+   log_error $(cat $ERROR_LOG)
+fi
 
 #=================================== EOF ======================================
