63ab0ce344ca63b1ad28817d23ddc264bd4f2562
Ulrich Becker <ulrich@linux1.sphairon.box>
2024-06-12 18:55:13 +0200

    build-DU.sh improved

--- tools/bash/build-DU.sh
+++ tools/bash/build-DU.sh
@@ -32,6 +32,44 @@ die()
    exit 1;
 }
 
+if [ "${HOSTNAME:0:6}" != "asl745" ]
+then
+   if [ ! -n "$OECORE_SDK_VERSION" ]
+   then
+      if [ "${HOSTNAME:0:5}" = "asl75" ]
+      then
+         YOKTO_SDK_PATH=/common/usr/embedded/yocto/fesa/current/sdk/
+      else
+         if [ ! -n "$YOKTO_SDK_PATH" ]
+         then
+            die "YOKTO_SDK_PATH is not defined!"
+         fi
+      fi
+      if [ ! -d "$YOKTO_SDK_PATH" ]
+      then
+         die "Directory for Yocto-SDK: \"$YOKTO_SDK_PATH\" not found!"
+      fi
+      if [ ! -n "$SDK_SETUP_FILE" ]
+      then
+         SDK_SETUP_FILE="environment-setup-core2-64-ffos-linux"
+      fi
+      if [ ! -f "${YOKTO_SDK_PATH}${SDK_SETUP_FILE}" ]
+      then
+         die "SDK- setup file \"${YOKTO_SDK_PATH}${SDK_SETUP_FILE}\" for Yocto not found!"
+      fi
+      unset $LD_LIBRARY_PATH
+      source ${YOKTO_SDK_PATH}${SDK_SETUP_FILE}
+   fi
+   echo "Build by Yocto-SDK version: \"$OECORE_SDK_VERSION\""
+else
+   echo "Build on ACC7."
+fi
+
+if [ "$1" = "clean" ]
+then
+   MAKE_ARG="clean"
+fi
+
 if [ ! -n "$TARGET" ]
 then
    die "Variable \"TARGET\" is not defined!"
@@ -72,6 +110,11 @@ then
    EXPORT_DIR=/common/export/fesa/local/${TARGET}
 fi
 
+if [ ! -d "$EXPORT_DIR" ]
+then
+   die "Export directory \"$EXPORT_DIR\" not found!"
+fi
+
 if [ ! -n "$BINARY_SUFFIX" ]
 then
    BINARY_SUFFIX=_M
@@ -85,12 +128,32 @@ fi
 REL_BIN_DIR=build/bin/$(uname -m)
 BINARY_FILE=${REL_BIN_DIR}/${DU_NAME}${BINARY_SUFFIX}
 
+if [ ! -n "$JOBS" ]
+then
+   JOBS=8
+fi
+
 cd $FESA_CLASS_NAME
-make $1
+make -j$JOBS $MAKE_ARG
+if [ "$?" != "0" ]
+then
+   die "Can't build \"$FESA_CLASS_NAME\"!"
+fi
+
 cd $ORIGIN_DIR
 
+if [ "$1" = "C" ]
+then
+   echo -e $ESC_SUCCESS"\"$FESA_CLASS_NAME\" successful build!"$ESC_NORMAL
+   exit 0
+fi
+
 cd $DU_NAME
-make $1
+make -j$JOBS $MAKE_ARG
+if [ "$?" != "0" ]
+then
+   die "Can't build \"${DU_NAME}\"!"
+fi
 
 if [ "$1" = "clean" ]
 then
@@ -98,21 +161,44 @@ then
    exit 0
 fi
 
-if [ -x "$BINARY_FILE" ]
+if [ ! -x "$BINARY_FILE" ]
 then
-   if [ -d "$EXPORT_DIR" ]
-   then
-     if [ ! -d "${EXPORT_DIR}/${DU_NAME}${BINARY_DIR_SUFFIX}" ]
-     then
-        die "Target directory \"${EXPORT_DIR}/${DU_NAME}${BINARY_DIR_SUFFIX}\" not found! Maybe the first build process had not made by Eclipse."
-     fi
-     echo -e $ESC_SUCCESS"*** Build was successful, exporting executable: \"${DU_NAME}\" to \"${TARGET}\" ***"$ESC_NORMAL
-     cp -u ${BINARY_FILE} ${EXPORT_DIR}/${DU_NAME}${BINARY_DIR_SUFFIX}/${DU_NAME}
-   else
-     die "Export directory \"$EXPORT_DIR\" not found!" 1>&2
-   fi
-else
-   die "Binary \"$BINARY_FILE\" not found!" 1>&2
+   die "Binary \"$BINARY_FILE\" not found!"
+fi
+
+if [ ! -d "${EXPORT_DIR}/${DU_NAME}${BINARY_DIR_SUFFIX}" ]
+then
+   die "Target directory \"${EXPORT_DIR}/${DU_NAME}${BINARY_DIR_SUFFIX}\" not found! Maybe the first build process had not made by Eclipse."
+fi
+
+echo -e $ESC_SUCCESS"*** Build was successful, exporting executable: \"${DU_NAME}\" to \"${TARGET}\" ***"$ESC_NORMAL
+cp -u ${BINARY_FILE} ${EXPORT_DIR}/${DU_NAME}${BINARY_DIR_SUFFIX}/${DU_NAME}
+
+TEST_DIR=src/test
+if [ ! -d "$TEST_DIR" ]
+then
+   die "Dierctory \"$TEST_DIR\" not found!"
+fi
+
+INSTANCE_FILE_DIR=${TEST_DIR}/${TARGET}
+if [ ! -d "$INSTANCE_FILE_DIR" ]
+then
+   die "Dierctory \"$INSTANCE_FILE_DIR\" not found!"
+fi
+
+INSTANCE_FILE=${INSTANCE_FILE_DIR}/DeviceData_${DU_NAME}.instance
+if [ ! -f "$INSTANCE_FILE" ]
+then
+   die "Instance file \"$(pwd)/${INSTANCE_FILE}\" not found!"
+fi
+
+echo "Exporting \"${DU_NAME}.instance\" to \"${TARGET}\""
+cp -u $INSTANCE_FILE ${EXPORT_DIR}/${DU_NAME}${BINARY_DIR_SUFFIX}/${DU_NAME}.instance
+if [ "$?" = "0" ]
+then
+   echo "Content of \"${EXPORT_DIR}/${DU_NAME}${BINARY_DIR_SUFFIX}\":"
+   ls -g ${EXPORT_DIR}/${DU_NAME}${BINARY_DIR_SUFFIX}
+   echo "done"
 fi
 cd $ORIGIN_DIR
 #=================================== EOF ======================================
