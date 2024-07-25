13dce42ea2fd515ed3d7b41795de8e1aac486371
Ulrich Becker <u.becker@gsi.de>
2024-07-25 14:48:00 +0200

    FOR_EACH_FB_CHANNEL runs in FESA-class PowerSupply

--- prj/scu-control/daq/linux/scu_fg_feedback.hpp
+++ prj/scu-control/daq/linux/scu_fg_feedback.hpp
@@ -1885,14 +1885,10 @@ void FgFeedbackAdministration::MilDaqAdministration::onDataReadingPause( void )
  *                       for each loop pass. This pointer is only within
  *                       this loop visible.
  */
-#define FOR_EACH_FB_CHANNEL( fbAdmin, channelPtrName )              \
-   static_assert( std::is_class<TYPEOF(fbAdmin)>::value,            \
-      "Macro argument fbAdmin is not a class!" );                   \
-   static_assert( std::is_base_of<TYPEOF(fbAdmin),                  \
-                                  FgFeedbackAdministration>::value, \
-      "Class of macro argument fbAdmin has not the base-class"      \
-      " FgFeedbackAdministration !" );                              \
-   for( const auto& __pDev: fbAdmin )                               \
+#define FOR_EACH_FB_CHANNEL( fbAdmin, channelPtrName )               \
+   static_assert( std::is_class<TYPEOF(fbAdmin)>::value,             \
+      "Macro argument " #fbAdmin " is not a instance of a class!" ); \
+   for( const auto& __pDev: fbAdmin )                                \
       for( auto& channelPtrName: *__pDev )
 
 ///////////////////////////////////////////////////////////////////////////////
