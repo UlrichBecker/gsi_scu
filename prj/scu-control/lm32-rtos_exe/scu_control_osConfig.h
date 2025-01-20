2e6f1a89fde7036b7ad7974baa0aaac94d8a78be
Ulrich Becker <u.becker@gsi.de>
2025-01-20 17:26:31 +0100

    task change frequency = 10kHz

--- prj/scu-control/lm32-rtos_exe/scu_control_osConfig.h
+++ prj/scu-control/lm32-rtos_exe/scu_control_osConfig.h
@@ -23,12 +23,12 @@
 /*
  * Minimal stack size is the stack size of the idle-task in 32-bit words as well.
  */
-#define configMINIMAL_STACK_SIZE                128 //64
+#define configMINIMAL_STACK_SIZE                128
 
 #define configUSE_PREEMPTION                    1
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION 0
 #define configUSE_TICKLESS_IDLE                 1
-#define configTICK_RATE_HZ                      5000 //10000 //20000
+#define configTICK_RATE_HZ                      10000 //20000
 #define configMAX_PRIORITIES                    2
 #define configMAX_TASK_NAME_LEN                 16
 #define configIDLE_SHOULD_YIELD                 1
